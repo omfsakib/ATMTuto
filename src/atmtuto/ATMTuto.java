360234d9822977a858e5ce1726e81fc5a15edab1
Ahmed Sakib <55790263+omfsakib@users.noreply.github.com>
2021-07-28 11:59:51 +0600

    All Done

--- src/atmtuto/ATMTuto.java
+++ src/atmtuto/ATMTuto.java
@@ -15,7 +15,7 @@ public class ATMTuto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new LOGIN().setVisible(true);
     }
     
 }
